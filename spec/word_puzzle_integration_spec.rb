d4e7342700d3edc0e1c84e39a70abd0fd7ae447d
Kyle <altck8@gmail.com>
2016-01-08 08:47:05 -0800

    includes integration testing for a multiple word entry

--- spec/word_puzzle_integration_spec.rb
+++ spec/word_puzzle_integration_spec.rb
@@ -4,8 +4,14 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the coin change path', {:type => :feature}) do
-  it('processes the user input and returns thier change') do
+describe('the word change path', {:type => :feature}) do
+  it('processes the user input and returns thier word with a - for all vowels') do
+    visit('/')
+    fill_in('word', :with => "Believe")
+    click_button('send')
+    expect(page).to have_content("B-l--v-")
+  end
+  it('processes the user input and returns thier words with a - for all vowels') do
     visit('/')
     fill_in('word', :with => "Believe you can and you're halfway there. Theodore Roosevelt")
     click_button('send')
