0460d0d5af240e26ba99d04f3fc939019fc5c3a0
Kyle <altck8@gmail.com>
2016-01-08 08:34:11 -0800

    includes rspec test for single word

--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -5,4 +5,7 @@ describe("String#word_puzzle") do
   it("replaces the vowels in a word with -") do
     expect("areas".word_puzzle).to(eq("-r--s"))
   end
+  it("replaces the vowels in a sentence of words with -") do
+    expect("Believe you can and you're halfway there. Theodore Roosevelt".word_puzzle).to(eq("B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt"))
+  end
 end
